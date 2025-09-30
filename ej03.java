eb64182fe0e0bd4df95f82fcd6fecfc5cdb04489
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-09-30 16:46:59 +0000

    Ejercicio 03 terminado

---
+++ ej03.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ej03 {
+    	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+System.out.println("por favor, introduzca su edad");
+int edad = sc.nextInt();	
+	
+        System.out.println("Tienes " + edad + " años.");
+    }
+}
+
