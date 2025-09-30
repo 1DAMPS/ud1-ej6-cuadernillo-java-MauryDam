de45e15f7bd042cd5e4daf97a8b90d84568445b5
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-09-30 16:43:09 +0000

    Ejercicio 02 termiando

---
+++ ej02.java
@@ -0,0 +1,10 @@
+public class ej02 {
+    public static void main(String[] args) {
+
+	String M = "Maury";
+    int E = 39;
+
+        System.out.println("Mi nombre es" + M);
+        System.out.println("Mi edad es" + E);
+    }
+}
