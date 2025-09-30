7f82794096a28991b07fd499e0c6f44cc6f76edf
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-09-30 16:56:32 +0000

    Ejercicio 05 terminado

---
+++ ej05.java
@@ -0,0 +1,17 @@
+/*De los siguientes nombres de variables, indica cuáles son válidos y cuáles no:
+ 2edad, _curso, while, Nombre, precioTotal.*/
+
+public class ej05 {
+    public static void main(String[] args) {
+        // Variables válidas
+        int _curso = 5;
+        String Nombre = "Maury";
+        double precioTotal = 10.99;
+
+        // Estas son las variables Válidas
+        System.out.println("Variables válidas:");
+        System.out.println("Curso: " + _curso);
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("Precio Total: " + precioTotal);
+    }
+}
