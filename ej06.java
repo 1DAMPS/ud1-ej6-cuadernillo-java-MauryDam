5ac2d60fb4234fbf693f4cca589fa7a57bbb60ea
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-09-30 17:13:53 +0000

    Ejercicio 06 terminado

---
+++ ej06.java
@@ -0,0 +1,16 @@
+/*ej06.java
+Escribe tres variables con nombres correctos siguiendo la notación camelCase y muestra sus valores.*/ 
+
+public class ej06{
+    public static void main(String[] args) {
+
+// Variables correctas declarando su valor
+        int edadActual = 39;
+        String nombreCorto = "Maury";
+        double precioTotal = 10.99;
+
+            System.out.println("Edad: " + edadActual);
+            System.out.println("Nombre: " + nombreCorto);
+            System.out.println("Precio: " + precioTotal);
+    }
+}
