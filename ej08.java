7029b8a5005d929393c66f25d0999ea31fbb98be
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-01 14:58:08 +0000

    Ejercicio 08 terminado

---
+++ ej08.java
@@ -0,0 +1,21 @@
+// Nombre clase
+public class ej08{
+
+// Metodo main donde ejecutamos el programa
+    public static void main(String[]args){
+
+// Declaramos las variables con sus valores respectivos
+int edad = 39;
+double precio = 10.90;
+String nombre ="Maury";
+boolean esVerdadero = true;
+char m = 'M';
+
+// Imprimimos por pantalla el valor de las variables
+    System.out.println("el valor de la edad es:" + edad);
+	System.out.println("el valor del precio  es:" + precio);
+	System.out.println("el valor de nombre es:" + nombre);
+	System.out.println("el valor del booleano es:" + esVerdadero);
+    System.out.println("el valor de m es:"  + m);
+}
+}
