ed4704f8abb2cbff892008dc7f6068b910e5b0ac
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-01 14:50:50 +0000

    Ejercicio 07 terminado

---
+++ ej07.java
@@ -0,0 +1,13 @@
+
+// Nombre de la clase
+public class ej07{
+
+//metodo para imprimir un mensaje
+public static void mostrarMensaje(){
+	System.out.println("Este es el metodo");
+}
+public static void main(String[]args){
+	mostrarMensaje();
+}
+}
+
