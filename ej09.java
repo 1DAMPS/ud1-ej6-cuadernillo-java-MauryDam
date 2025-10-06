b80054d2b50a46f233b811fa9af13a22f58aa6ea
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-06 14:43:58 +0000

    Ejercicio 09 Terminado

---
+++ ej09.java
@@ -0,0 +1,19 @@
+// Clase con el nombre del ejercicio
+public class ej09 {
+// Metodo main
+	public static void main(String[]args){
+//Declaracion variable sin inicializar
+int edad;
+	
+System.out.println("mi edad es :” + edad
+}
+
+/*ME SALE ESTE ERROR EN LA CONSOLA:
+@MauryDam ➜ /workspaces/ud1-ej6-cuadernillo-java-MauryDam (main) $ javac ej09.java
+ej09.java:8: error: unclosed string literal
+System.out.println("mi edad es :” + edad
+                   ^
+ej09.java:9: error: reached end of file while parsing
+}
+ ^
+2 errors*/
