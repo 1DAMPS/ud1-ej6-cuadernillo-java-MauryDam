5ffbbb9f8722ba9783d82ba997c7748f379116f4
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-06 14:53:03 +0000

    Ejercicio terminado

---
+++ ej11.java
@@ -0,0 +1,11 @@
+// Nombre de la clase
+public class ej11{
+// Metodo main
+	public static void main(String[]args){
+
+// declaramos variable
+long l = 8000000000L;
+
+	System.out.println("La población mundial en la actualidad es de :" + l);
+}
+}
