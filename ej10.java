7f21f6c050974ce2d69645351734e10d785c08ea
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-06 14:46:34 +0000

    Ejercicio 10 terminado

---
+++ ej10.java
@@ -0,0 +1,13 @@
+// Nombre de la clase
+public class ej10{
+//Metodo main
+	public static void main(String[]args){
+
+//Declaracion e inicializacion de variables
+byte b = 39;
+short  s = 100;
+
+	System.out.println("el valor de byte es :" + b);
+	System.out.println("el valor de short es :" + s);
+}
+}
