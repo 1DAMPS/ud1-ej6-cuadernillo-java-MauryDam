04e1a9362954e4d8aa35c05d2b97b4933d0b3310
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-06 15:07:22 +0000

    Ejercicio 14 terminado

---
+++ ej14.java
@@ -0,0 +1,10 @@
+// Nombre de la clase
+public class ej14{
+	public static void main(String[]args){
+// Declaracion de variable constante
+final int DIAS_SEMANA = 7;
+// Imprimimos por pantalla el valor de la variable constante
+	System.out.println("los dias de la semana son:  " + DIAS_SEMANA);
+}
+}
+
