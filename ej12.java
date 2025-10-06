f9b398e2cdce48d93e0cde80e96e503f3b28d801
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-06 14:56:27 +0000

    Ejercicio terminado

---
+++ ej12.java
@@ -0,0 +1,16 @@
+// nombre clase
+public class ej12{
+	public static void main(String[]args){
+
+float f = 3.14;
+
+		System.out.println("cual es el valor del numero pi?  :");
+	    System.out.println("el valor de numero pi es :" + f);
+}
+}	
+/*ME SALE ESTE MENSAJE DE ERROR:
+@MauryDam ➜ /workspaces/ud1-ej6-cuadernillo-java-MauryDam (main) $ javac ej12.java
+ej12.java:5: error: incompatible types: possible lossy conversion from double to float
+float f = 3.14;
+          ^
+1 error. Me recalca que hace falta una "f" asiganada a la variable para indicar que es de tipo "float"*/ */
