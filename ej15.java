d80c7aab2ad4c1facfc48a4d72dc47b06794cba2
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-07 08:16:39 +0000

    Ejercicio 15 terminado

---
+++ ej15.java
@@ -0,0 +1,9 @@
+public class ej15{ //Asignamos nombre a la clase
+    public static void main(String[]args){
+        // Declaracion de variable asignando valor
+        int num = 2147483647;
+        int desborde = num + 1;
+        // Imprimimos por pantalla el valor de la variable desborde
+        System.out.println("El valor de desborde es: " + desborde);
+    }
+}
