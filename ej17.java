b3c1d45120f73ac079e9f149c18159c448e03520
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-07 09:23:11 +0000

    Ejercicio 17 terminado

---
+++ ej17.java
@@ -0,0 +1,7 @@
+/*Ej17.txt
+Reflexiona: ¿por qué es importante elegir el tipo de dato adecuado según 
+el rango de valores que necesitas manejar?
+Es fundamental utilizar en nuestra aplicación el tipo de dato correcto, o más adecuado
+para garantizar el funcionamiento de la propia aplicacion, ya que se pueden producir problemas
+de desbordamiento o datos incorrectos dentro de la misma. Supongo que tambien el tipo de dato
+adecuado, permite hacer un uso más de la memoria disponible*/
