09cdc8d4a8e4adb28bda348b39417a4ebdd22423
MauryDam <166145527+MauryDam@users.noreply.github.com>
2025-10-07 09:42:57 +0000

    Ejercicio 19 terminado

---
+++ ej19.java
@@ -0,0 +1,11 @@
+
+public class ej19{
+    public static void main(String[]args){
+       
+        // Declaramos y asignamos valor a la variable radio
+        int radio = 5;
+        //calculamos el area de un circulo, asignando una operacion
+        // a la variable area
+        double area = 3.1416 * radio * radio;
+        // Imprimimos por pantalla el valor de la variable area
+        System.out.println(area);
